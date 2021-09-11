022fba15d8681fde6eb9555a599d0baf1fe5773c
Marcus Paulo <marcuscarvalho0608@gmail.com>
2021-09-10 23:43:23 -0300

    Adicionar arquivos de projeto.

---
+++ Site/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site
+{
+    public class TodoItem
+    {
+        public string Titulo { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
